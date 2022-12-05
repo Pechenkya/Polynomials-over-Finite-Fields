5fdb3dfb653eef4870ba2745861f7c04d0b47aa5
Peter Bondar <bondar.petro@lll.kpi.ua>
2022-12-05 13:49:29 +0200

    temp-commit

---
+++ PolynomialsOFF/POFF_Variant_2.cpp
@@ -0,0 +1,1 @@
+#include "POFF_Variant_2.hpp"
---
+++ PolynomialsOFF/POFF_Variant_2.hpp
@@ -0,0 +1,12 @@
+// Include area
+
+#define FIELD_POWER 173
+
+/**
+ * Class to describe Galois Fields with charasteristics  
+ * Irreducible polynomial to generate the field: p(x) = x^173 + x^10 + x^2 + x + 1
+ */
+class PB_Polynomial
+{
+
+};
